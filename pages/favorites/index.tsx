6d6c8f94c5554008811928643e7d74ca87e2389e
cristiansep <cristiansepulvedamendez@gmail.com>
2022-03-13 17:59:02 -0300

    First commit

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,32 @@
+
+import { Layout } from '../../components/layouts'
+import { NoFavorites } from '../../components/ui'
+import { useEffect, useState } from 'react';
+import { localFavorites } from '../../utils';
+import { FavoritePokemons } from '../../components/pokemon';
+
+const FavoritesPage = () => {
+
+
+  const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
+
+  useEffect(() => {
+    setFavoritePokemons(localFavorites.pokemons());
+  }, [])
+  
+
+
+  return (
+    <Layout title="Favoritos">
+      {
+        favoritePokemons.length === 0
+        ? <NoFavorites/>
+        : <FavoritePokemons favoritePokemons={favoritePokemons} />
+        
+      }
+     
+    </Layout>
+  )
+}
+
+export default FavoritesPage
